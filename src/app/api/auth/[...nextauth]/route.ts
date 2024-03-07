3878e0b18130637042cf0aa683252bf44aa2b75f
Songhyeon Jun <shjeon0730@gmail.com>
2024-03-07 00:44:59 -0600

    testing login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,4 @@ import NextAuth from 'next-auth';
 import { authOptions } from '@/app/lib/auth';
 
 const handler = NextAuth(authOptions);
-export const GET = handler;
-export const POST = handler; //{ handler as GET, handler as POST };
+export { handler as GET, handler as POST };
