95ffaa097d22bd3384cf10c809223c420490aac8
Songhyeon Jun <shjeon0730@gmail.com>
2024-03-07 08:30:38 -0600

    remove vercel URL from nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
         FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET,
-        VERCEL_URL: process.env.VERCEL_URL,
         NEXTAUTH_URL: process.env.VERCEL_URL,
     },
 };
