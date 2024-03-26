116b6dd096838d41e5c9c08d2f5c778e23f90014
Songhyeon Jun <shjeon0730@gmail.com>
2024-03-26 00:55:06 -0500

    fix bug

--- src/app/api/admin/revalidate-google/route.ts
+++ src/app/api/admin/revalidate-google/route.ts
@@ -4,8 +4,6 @@ import { GOOGLE_REVALIDATE_ID } from '@/lib/google-sheets/consts';
 import { revalidateTag } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
-export default async function action() {}
-
 export async function GET(req: NextRequest, _res: NextResponse<{}>) {
     revalidateTag(GOOGLE_REVALIDATE_ID);
     return NextResponse.json({ message: 'ok' });
