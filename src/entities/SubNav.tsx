12c0319af5c6349481ffe8098dfa6b302c66cd5e
Songhyeon Jun <shjeon0730@gmail.com>
2024-03-26 23:04:55 -0500

    sub nav sticky fix

--- src/entities/SubNav.tsx
+++ src/entities/SubNav.tsx
@@ -11,21 +11,19 @@ type Props<T extends string = string> = {
 };
 const SubNav = <T extends string = string>({ navList, titles }: Props<T>) => {
     return (
-        <header className='sticky top-0 gap-4 z-50 flex flex-row items-center px-4 text-sm font-medium bg-black/80 border-b border-gray-200 dark:bg-gray-950 dark:border-gray-800'>
-            <nav className='flex flex-row gap-4 text-white'>
-                {navList.map(nav => (
-                    <NavLink
-                        href={nav.href}
-                        key={nav.href}
-                        exact={nav.exact}
-                        onlyHorizontal={true}
-                        notReady={nav.notReady}
-                    >
-                        {titles ? titles[nav.title as T] : nav.title}
-                    </NavLink>
-                ))}
-            </nav>
-        </header>
+        <nav className='sticky top-14 sm:top-16 md:top-20 gap-4 z-50 flex flex-row flex-wrap text-white items-center px-4 text-sm font-medium bg-black/80 border-b border-gray-200 dark:bg-gray-950 dark:border-gray-800'>
+            {navList.map(nav => (
+                <NavLink
+                    href={nav.href}
+                    key={nav.href}
+                    exact={nav.exact}
+                    onlyHorizontal={true}
+                    notReady={nav.notReady}
+                >
+                    {titles ? titles[nav.title as T] : nav.title}
+                </NavLink>
+            ))}
+        </nav>
     );
 };
 
