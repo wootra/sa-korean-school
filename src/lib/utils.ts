a9735a39ebd6876500f7a29e5c9d78551030ee14
Songhyeon Jun <shjeon0730@gmail.com>
2024-11-13 19:33:25 -0600

    fix url bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,8 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-export function stripUrl(url: string) {
+export function stripUrl(url?: string) {
+	if (!url) return '';
 	const protocol = url.split('://')[0];
 	const onlyUrl = url.split('://')[1];
 	const baseUrl = onlyUrl.split('/')[0];
